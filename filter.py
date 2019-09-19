7bd46da56b579f0e0c7fa43ca32b769c703579a3
Bernd Porr <bernd.porr@glasgow.ac.uk>
2019-09-19 11:21:56 +0000

    Added unit test to the filter and it seems to work! :)

--- filter.py
+++ filter.py
@@ -6,8 +6,8 @@ class SecondOrderFilter:
     processing with one sample in and one sample out. No arrays are
     used for processing. Just simple variables.
     '''
-
-    __init__(self):
+    
+    def __init__(self):
         self.hold = 0
         self.hold1 = 0
         self.hold2 = 0
@@ -30,74 +30,98 @@ class SecondOrderFilter:
         '''
         Performs the filter operation sample by sample.
         '''
-	    self.hold = input - b1 * self.hold1 - b2 * self.hold2
-	    self._output = a0 * self.hold + a1 * self.hold1 + a2 * self.hold2
-	    self.hold2 = self.hold1
-	    self.hold1 = self.hold
-	return self._output
+        self.hold = input - self.b1 * self.hold1 - self.b2 * self.hold2
+        self._output = self.a0 * self.hold + self.a1 * self.hold1 + self.a2 * self.hold2
+        self.hold2 = self.hold1
+        self.hold1 = self.hold
+        return self._output
 
-    init_with_zero_pole(self, gain, zero, pole):
+    def init_with_zero_pole(self, gain, zero, pole):
         '''
         Valid second order filters have poles and zeros which are complex conjugates of each other
         only need one zero and one pole as parameter.
         '''
         self.a0 = gain
         self.a1 = -2 * gain * zero.real
-        self.a2 = gain * ( zero.real * zero.real + zero.imag * zero.imag
+        self.a2 = gain * ( zero.real * zero.real + zero.imag * zero.imag )
         self.b1 = -2 * pole.real
         self.b2 = pole.real * pole.real + pole.imag * pole.imag
         self.hold1 = 0
         self.hold2 = 0
 
-    init_lowpass(self, frequency, qFactor):
+    def init_lowpass(self, frequency, qFactor = 0.49):
         '''
         Setup a lowpass filter with normalised frequency and Q factor (>0.5)
         '''
-	    Omega = 2 * math.tan( math.pi * frequency )
-	    Omega2 = Omega * Omega
+        Omega = 2 * math.tan( math.pi * frequency )
+        Omega2 = Omega * Omega
         c0 = 2 * Omega / qFactor
-	    c1 = 4 + self.c0 + Omega2
-	    self.a0 = Omega2 / c1
-	    self.a1 = 2 * self.a0
-	    self.a2 = self.a0
-	    self.b1 = 2 * (Omega2 - 4) / c1
-	    self.b2 = (4 - c0 + Omega2) / c1
+        c1 = 4 + c0 + Omega2
+        self.a0 = Omega2 / c1
+        self.a1 = 2 * self.a0
+        self.a2 = self.a0
+        self.b1 = 2 * (Omega2 - 4) / c1
+        self.b2 = (4 - c0 + Omega2) / c1
 
-    init_highpass(self,frequency, qFactor):
+    def init_highpass(self,frequency, qFactor = 0.71):
         '''
         Setup a highpass filter with normalised frequency and Q factor (>0.5)
         '''
-	    Omega = 2 * math.tan( math.pi * frequency )
-	    Omega2 = Omega * Omega
+        Omega = 2 * math.tan( math.pi * frequency )
+        Omega2 = Omega * Omega
 
-	    c0 = 2 * Omega / qFactor
-	    c1 = 4 + self.c0 + Omega2
+        c0 = 2 * Omega / qFactor
+        c1 = 4 + self.c0 + Omega2
 
-	    self.a0 = 4 / c1
-	    self.a1 = -2 * self.a0
-	    self.a2 = self.a0
+        self.a0 = 4 / c1
+        self.a1 = -2 * self.a0
+        self.a2 = self.a0
 
-	    self.b1 = 2 * (Omega2 - 4) / c1
-	    self.b2 = (4 - c0 + Omega2) / c1
+        self.b1 = 2 * (Omega2 - 4) / c1
+        self.b2 = (4 - c0 + Omega2) / c1
 
-    normalise(self, frequency, qFactor, stepFunction = False):
+    def normalise(self, frequency, qFactor, stepFunction = False):
         '''
         Normalise the output with a delta pulse (default) or
         a step function by adjusting the FIR coefficients.
         '''
         self.init_lowpass(frequency, qFactor)
-	    max = self.filter(1)
+        max = self.filter(1)
 
         inputValue = 0
         if stepFunction:
             inputValue = 1
 
-	    while self.filter(inputValue) >= max:
-		    max = self._output
+        while self.filter(inputValue) >= max:
+    	    max = self._output
 
         if max > 0:
-	        a0 /= max
-	        a1 /= max
-	        a2 /= max
+            a0 /= max
+            a1 /= max
+            a2 /= max
         
         self.resetState()
+
+
+def calc_impulse_response(filename,lp):
+    f = open(filename,"w")
+    lp.filter(0)
+    v = lp.filter(1)
+    for i in range(1000):
+        f.write(str(v))
+        f.write("\n")
+        v = lp.filter(0)
+    f.close
+
+
+
+def unitTest():
+    lp = SecondOrderFilter()
+    lp.init_lowpass(0.1,1)
+    calc_impulse_response("impulse_q1.dat",lp)
+    lp = SecondOrderFilter()
+    lp.init_lowpass(0.1)
+    calc_impulse_response("impulse_damped.dat",lp)
+
+if __name__ == "__main__":
+    unitTest()
